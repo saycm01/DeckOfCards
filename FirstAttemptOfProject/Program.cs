110c97498b3461a580a229848a3d93785f5fb4ab
saycm01 <callumsay2@gmail.com>
2016-07-18 22:24:40 +0100

    Add files via upload
    This code is messing about with changing the background colour and resetting the colour of outputs.

---
+++ FirstAttemptOfProject/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstAttemptOfProject
+{
+    class Deck
+    {
+        
+
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Suits = new List<string>();
+            Suits.Add("Spades");
+            Suits.Add("Hearts");
+            Suits.Add("Clubs");
+            Suits.Add("Diamonds");
+            int length = Suits.Count -1;
+
+            for (int counter = 0; counter <= length; counter++)
+            {
+                if (counter % 2 != 0) 
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine(Suits[counter]);
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ResetColor();
+                    Console.WriteLine(Suits[counter]);
+                }
+            }
+            
+
+
+
+
+
+            /*
+            Console.WriteLine(Suits[0]);
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine(Suits[1]);
+            Console.ResetColor();
+            Console.WriteLine(Suits[2]);
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine(Suits[3]);
+            Console.ResetColor();
+            */
+        }
+    }
+}
